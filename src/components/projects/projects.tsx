f935c020fe26cca583a8227cc139b96b53784d35
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-08 17:47:44 -0300

    feature component project

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -4,7 +4,7 @@ import styles from './project.module.scss';
 import logo_costs from '../../images/Costs_resized.png';
 import logo_apiGit from '../../images/api_github.png';
 import logo_landingPage from '../../images/cleanLanding.png';
-import logo_huddle from '../../images/page_huddle.png';
+import logo_huddle from '../../images/lista-compras.png';
 
 
 const Projects = () => {
@@ -50,20 +50,20 @@ const Projects = () => {
                         <p className={styles.coment}>Landing Page Download 'APP'</p>
 
                         <div className={styles.hover_content}>
-                            <p className={styles.coment_1}>Esse é um projeto para usuários da apple interagir e baixar o conteúdo.</p>
+                            <p className={styles.coment_1}>WebSite para usuários da apple interagir e baixar o conteúdo.</p>
                             <p>🔗 Ver projeto online</p>
                         </div>
                     </a>
                 </div>
                 <div className={`${styles.project} ${styles.second}}`}>
-                    <a href='https://yanndarkz.github.io/Landing-page-Huddle/'
+                    <a href='https://shopping-list-gilt-alpha.vercel.app/'
                         target='_blank'
                         rel="noopener noreferrer">
                         <img src={logo_huddle} alt="Logo-projeto" />
-                        <p className={styles.coment}>Landing Page Huddle</p>
+                        <p className={styles.coment}>Lista de Compras</p>
 
                         <div className={styles.hover_content}>
-                            <p className={styles.coment_1}>Esse é um projeto para apresentação de uma rede social do frontend mentor</p>
+                            <p className={styles.coment_1}>Esse projeto tem o intuito de armazenar e gerenciar uma lista de compras de Super Mercado</p>
                             <p>🔗 Ver projeto online</p>
                         </div>
                     </a>
